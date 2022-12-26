2ccc6aa0ee1d962f4353caad93a54c4333233c54
c0de_h0ng <ghdtjrdn0323@gmail.com>
2022-12-26 21:11:15 +0900

    깃허브 라이브러리 설정

--- app/src/main/java/com/c0deh0ng/sample/clonebankaccount/MainActivity.kt
+++ app/src/main/java/com/c0deh0ng/sample/clonebankaccount/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.c0deh0ng.sample.clonebankaccount
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
